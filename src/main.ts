34f1477be7b4140a964b370865e977b0ea5ad50e
XOTlC <twilightgeometrydash@gmail.com>
2024-04-18 20:47:33 +0000

    added admin.blacket.org to cors

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,7 @@ async function bootstrap() {
         origin: [
             /* to config, put your own domain here include http[s]:// */
             "https://rewrite.blacket.org",
+            "https://admin.blacket.org",
             "https://blacket.org"
         ],
         credentials: true
